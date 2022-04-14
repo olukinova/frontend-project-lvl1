5a2d5a6051c3aa0deec0de29913cf08314ab5929
Olena Lukinova <olena70220@gmail.com>
2022-04-13 23:34:17 -0700

    create and test game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/olukinova/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/olukinova/frontend-project-lvl1/actions)
+
+![example workflow](https://github.com/<OWNER>/<REPOSITORY>/actions/workflows/<WORKFLOW_FILE>/badge.svg)
---
+++ bin/brain-even.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name?: ');
+console.log(`Hello,  ${userName}!`);
+const generateRandomNunber = (min, max) => Math.round(Math.random() * (max - min) + min);
+const isEven = (number) => number % 2 === 0;
+
+console.log('Answer "yes" if the number is even, otherwise answer "no"');
+
+const startRound = () => {
+  const number = generateRandomNunber(0, 10);
+  console.log(`Question: ${number}`);
+  const userAnswer = readlineSync.question('Your Answer: ');
+  console.log(userAnswer);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  if (userAnswer === correctAnswer) {
+    console.log('Correct!');
+    return true;
+  }
+  console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
+  return false;
+};
+
+const startGame = () => {
+  const numberOfRounds = 3;
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    const result = startRound();
+    if (result === false) {
+      console.log(`Let's try again, ${userName}`);
+      return;
+    }
+  }
+console.log(`Congratulations, ${userName}!`);
+};
+startGame();
