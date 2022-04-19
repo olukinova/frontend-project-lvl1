a1ce7856e8b34de5042a96ab9b6c4a15b24b54f0
Olena Lukinova <olena70220@gmail.com>
2022-04-19 10:29:28 -0700

    cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,6 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
-export default const userName = readlineSync.question('May I have your name?: ');
+const userName = readlineSync.question('May I have your name?: ');
+
+export default userName;
